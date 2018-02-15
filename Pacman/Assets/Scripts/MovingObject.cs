d227ab406d8ae668d24a63016c18016a78c9bf85
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-02-14 21:45:58 -0300

    Personaje moviendose

---
+++ Pacman/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float movingSpeed;
+
+    protected Rigidbody2D rb2D;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+    protected void Move(Vector2 movement)
+    {
+        rb2D.velocity = movement*movingSpeed;
+    }
+
+
+}
---
+++ Pacman/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+    
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(movementX, movementY);
+       // StopMoving(movementX, movementY);
+        Move(movement);
+    }
+
+    void StopMoving(float x, float y)
+    {
+        float xVelocity = (x == 0) ? 0 : rb2D.velocity.x;
+        float yVelocity = (y == 0) ? 0 : rb2D.velocity.y;
+
+        rb2D.velocity = new Vector2(xVelocity, yVelocity);
+    }
+}
